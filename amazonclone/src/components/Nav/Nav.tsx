d7953cb042fc007deb88951c19e325d5f3c7dff2
Jonatahn Bajada <jbajada@ryerson.ca>
2021-06-13 00:30:02 -0700

    added nav component

---
+++ amazonclone/src/components/Nav/Nav.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Nav = () => {
+	return (
+		<div>
+			{/* Top Nav  */}
+			<div style={{ height: 40, backgroundColor: '#131921' }} />
+
+			{/* Second header  */}
+			<div style={{ height: 35, backgroundColor: '#232f3e' }} />
+		</div>
+	);
+};
+
+export default Nav;
