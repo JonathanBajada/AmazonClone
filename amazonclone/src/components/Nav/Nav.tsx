893ba57856317e6a6757e95c0728e13c06b4bb71
Jonatahn Bajada <jbajada@ryerson.ca>
2021-06-13 02:28:42 -0700

    added css file

--- amazonclone/src/components/Nav/Nav.tsx
+++ amazonclone/src/components/Nav/Nav.tsx
@@ -1,23 +1,28 @@
 import React from 'react';
+import './Nav.css';
 
 const Nav = () => {
 	return (
 		<div>
 			{/* Top Nav  */}
-			<div style={{ height: 60, backgroundColor: '#131921' }}>
+			<div style={{ height: 60, backgroundColor: '#131921', display: 'flex' }}>
 				{/* Left Nav  */}
-				<div style={{}}>
-					<button>Home</button>
-					<button>Deliver</button>
+				<div style={{ alignSelf: 'center' }}>
+					<button className="button">
+						<h1 style={{ color: 'white', fontSize: 14 }}>Home</h1>
+					</button>
+					<button className="button" style={{ height: 60 }}>
+						<h1 style={{ color: 'black', fontSize: 14 }}>Deliver</h1>
+					</button>
 				</div>
 
 				{/* Search Bar  */}
-				<div>
+				<div style={{ alignSelf: 'center', color: 'white' }}>
 					<h1>Search</h1>
 				</div>
 
 				{/* Right Nav  */}
-				<div>
+				<div style={{ alignSelf: 'center', marginLeft: 'auto' }}>
 					<button>Language</button>
 					<button>Sign In</button>
 					<button>Deliver</button>
