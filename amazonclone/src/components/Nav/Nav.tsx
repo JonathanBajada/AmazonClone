0e32ff64f9f1ff1d4211dd368a5e8be90886c7cc
Jonatahn Bajada <jbajada@ryerson.ca>
2021-06-13 00:37:26 -0700

    updated nav css

--- amazonclone/src/components/Nav/Nav.tsx
+++ amazonclone/src/components/Nav/Nav.tsx
@@ -4,7 +4,26 @@ const Nav = () => {
 	return (
 		<div>
 			{/* Top Nav  */}
-			<div style={{ height: 60, backgroundColor: '#131921' }} />
+			<div style={{ height: 60, backgroundColor: '#131921' }}>
+				{/* Left Nav  */}
+				<div style={{}}>
+					<button>Home</button>
+					<button>Deliver</button>
+				</div>
+
+				{/* Search Bar  */}
+				<div>
+					<h1>Search</h1>
+				</div>
+
+				{/* Right Nav  */}
+				<div>
+					<button>Language</button>
+					<button>Sign In</button>
+					<button>Deliver</button>
+					<button>Cart</button>
+				</div>
+			</div>
 
 			{/* Second header  */}
 			<div style={{ height: 39, backgroundColor: '#232f3e' }} />
