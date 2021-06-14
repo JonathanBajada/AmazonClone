c3b8e0afc9bd5510a32be00e934a0ec768704dd8
Jonatahn Bajada <jbajada@ryerson.ca>
2021-06-14 14:23:47 -0700

    updated amazon logo

--- client/src/components/Nav/Nav.tsx
+++ client/src/components/Nav/Nav.tsx
@@ -50,11 +50,68 @@ const Nav: React.FC<NavProps> = () => {
 				<SearchBar />
 
 				{/* Right Nav  */}
-				<div style={{ alignSelf: 'center', marginLeft: 'auto' }}>
-					<button>Language</button>
-					<button>Sign In</button>
-					<button>Deliver</button>
-					<button>Cart</button>
+				<div style={{ display: 'flex', flexDirection: 'row', marginLeft: 'auto' }}>
+					<div style={{ display: 'flex', flexDirection: 'column', alignSelf: 'center' }}>
+						<span
+							style={{
+								color: '#ccc',
+								fontSize: '12px',
+								fontWeight: 400,
+								lineHeight: '15px'
+							}}
+						>
+							Hello, Sign in
+						</span>
+						<span
+							style={{
+								color: 'white',
+								fontSize: '14px',
+								fontWeight: 700,
+								letterSpacing: '-0.5px',
+								lineHeight: '15px'
+							}}
+						>
+							Accounts & Lists
+						</span>
+					</div>
+
+					<div style={{ display: 'flex', flexDirection: 'column', alignSelf: 'center' }}>
+						<span
+							style={{
+								color: '#ccc',
+								fontSize: '12px',
+								fontWeight: 400,
+								lineHeight: '15px'
+							}}
+						>
+							Returns
+						</span>
+						<span
+							style={{
+								color: 'white',
+								fontSize: '14px',
+								fontWeight: 700,
+								letterSpacing: '-0.5px',
+								lineHeight: '15px'
+							}}
+						>
+							& Orders
+						</span>
+					</div>
+
+					<div style={{ display: 'flex', flexDirection: 'column', alignSelf: 'center' }}>
+						<span
+							style={{
+								color: 'white',
+								fontSize: '14px',
+								fontWeight: 700,
+								letterSpacing: '-0.5px',
+								lineHeight: '15px'
+							}}
+						>
+							Carts
+						</span>
+					</div>
 				</div>
 			</div>
 
