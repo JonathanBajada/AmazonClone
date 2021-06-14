86f0e65062b54ee4b57aee415e624dacd423dc6c
Jonatahn Bajada <jbajada@ryerson.ca>
2021-06-13 17:13:22 -0700

    code cleanup

--- client/src/components/Nav/Nav.tsx
+++ client/src/components/Nav/Nav.tsx
@@ -40,39 +40,3 @@ const Nav: React.FC<NavProps> = () => {
 };
 
 export default Nav;
-
-/*
-const Nav: React.FC<NavProps> = () => {
-	return (
-		<div>
-		
-			<div style={{ height: 60, backgroundColor: '#131921', display: 'flex' }}>
-			
-				<div style={{ alignSelf: 'center' }}>
-					<button className={styles.buttonStyle}>
-						<h1 style={{ color: 'white', fontSize: 14 }}>Home</h1>
-					</button>
-					<button className="button" style={{ height: 60 }}>
-						<h1 style={{ color: 'black', fontSize: 14 }}>Deliver</h1>
-					</button>
-				</div>
-
-				
-				<div style={{ alignSelf: 'center', color: 'white' }}>
-					<h1>Search</h1>
-				</div>
-
-			
-				<div style={{ alignSelf: 'center', marginLeft: 'auto' }}>
-					<button>Language</button>
-					<button>Sign In</button>
-					<button>Deliver</button>
-					<button>Cart</button>
-				</div>
-			</div>
-
-			
-			<div style={{ height: 39, backgroundColor: '#232f3e' }} />
-		</div>
-	);
-};*/
