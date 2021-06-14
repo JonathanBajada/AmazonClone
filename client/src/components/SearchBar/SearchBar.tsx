a9725b38e6f26ce7767bba7522c663d2f306060f
Jonatahn Bajada <jbajada@ryerson.ca>
2021-06-13 19:50:33 -0700

    update search bar css

--- client/src/components/SearchBar/SearchBar.tsx
+++ client/src/components/SearchBar/SearchBar.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 const SearchBar: React.FC = () => {
 	return (
-		<form action="/" method="get" style={{ alignSelf: 'center' }}>
+		<form action="/" method="get" style={{ alignSelf: 'center', display: 'flex' }}>
 			<input style={{ height: '38px' }} type="text" id="header-search" placeholder="Search blog posts" name="s" />
-			<button style={{ backgroundColor: '#FF9900', height: '45px' }} type="submit">
+			<button style={{ backgroundColor: '#FF9900', height: '45px', borderStyle: 'solid' }} type="submit">
 				Search
 			</button>
 		</form>
