a38afd85978334f1e69269ad3c9d5bd9f7e57812
Jonatahn Bajada <jbajada@ryerson.ca>
2021-06-14 17:29:44 -0700

    update search bar css

--- client/src/components/SearchBar/SearchBar.tsx
+++ client/src/components/SearchBar/SearchBar.tsx
@@ -53,32 +53,3 @@ const SearchBar: React.FC = () => {
 	);
 };
 export default SearchBar;
-
-{
-	/*<form action="/" method="get" style={{ alignSelf: 'center', display: 'flex' }}>
-			<div style={{ backgroundColor: borderColor, width: '100%', borderRadius: '5px', height: '50px' }}>
-				<div
-					style={{
-						display: 'inline-block',
-						margin: '0 auto;',
-						padding: '3px'
-					}}
-				>
-					<input
-						style={{ height: '38px', borderStyle: 'solid', borderColor: '#FF9900' }}
-						type="text"
-						id="header-search"
-						placeholder=""
-						name="s"
-						onClick={() => {
-							setBorderColor('#FF9900');
-						}}
-						onBlur={() => {
-							setBorderColor('#131921');
-						}}
-					/>
-				
-				</div>
-			</div>
-		</form>*/
-}
