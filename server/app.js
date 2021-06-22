37dbb4826e432f32588dfa53cadddcf71fd196d3
KC Kai <kirstenckai@gmail.com>
2021-06-22 14:29:54 -0700

    create product route and schema

--- server/app.js
+++ server/app.js
@@ -29,22 +29,7 @@ app.use(cors({require: true, credentials: true}));
 // routes
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/products', productsRouter);
+app.use('/', productsRouter);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
 
 module.exports = app;
--- server/routes/api/products.js
+++ server/routes/api/products.js
@@ -1,15 +1,27 @@
 const express = require('express');
+const Product = require('../../models/Product');
+
 const router = express.Router();
 
-const Product = require('../../models/product');
-
-router.get('/products', async(req, res) =>{
+router.get('/products', async (req, res) => {
   const products = await Product.find({});
 
   try {
     res.send(products);
-  } catch(error) {
-    res.status(500).send(error)
+  } catch (err) {
+    res.status(500).send(err);
+  }
+})
+
+// Route to create a new product. We won't be using this route until a seller user is implemented.
+router.post('/product', async (req, res) => {
+  const product = new Product(req.body);
+
+  try {
+    await product.save();
+    res.send(product);
+  } catch (err) {
+    res.status(500).send(err);
   }
 })
 
