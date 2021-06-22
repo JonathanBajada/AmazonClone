255fdd34773dbc31efd9999c081ac9d7c2e3837a
Jonatahn Bajada <jbajada@ryerson.ca>
2021-06-21 17:22:54 -0700

    updated css login form

--- client/src/components/Login/LoginPage.tsx
+++ client/src/components/Login/LoginPage.tsx
@@ -11,47 +11,39 @@ const LoginPage = () => {
 				<Link style={{ alignSelf: 'center' }} to={'/'}>
 					<img src={logo} style={{ width: '200px', height: '75px' }} />
 				</Link>
-				<div
-					style={{
-						display: 'flex',
-						justifyContent: 'center',
-						alignSelf: 'center',
-						borderRadius: '4px'
-					}}
-				>
-					<div className={styles.formBox}>
-						<h1>Sign In</h1>
 
-						<div id="email" style={{ marginBottom: '14px' }}>
-							<label htmlFor="ap_email">Enter email address</label>
-							<input
-								maxLength={128}
-								className={styles.inputText}
-								id="ap_email"
-								type="email"
-								name="email"
-								tabIndex={1}
-							/>
-						</div>
+				<div className={styles.formBox}>
+					<h1>Sign In</h1>
+
+					<div id="email" style={{ marginBottom: '14px' }}>
+						<label htmlFor="ap_email">Enter email address</label>
 						<input
+							maxLength={128}
 							className={styles.inputText}
-							style={{
-								display: 'block',
-								height: '23px',
-								borderRadius: '2px',
-								padding: '3px 7px 3px 7px',
-								lineHeight: 'normal',
-								width: '99.948%',
-								border: '1px solid #a6a6a6',
-								boxShadow: '0 1px 0 rgb(255 255 255 / 50%), 0 1px 0 rgb(0 0 0 / 7%) inset',
-								outline: '0',
-								//borderTopColor: '#949494;',
-								borderColor: '#e77600'
-							}}
-							type="submit"
-							name="submit"
+							id="ap_email"
+							type="email"
+							name="email"
+							tabIndex={1}
 						/>
 					</div>
+					<input
+						className={styles.inputText}
+						style={{
+							display: 'block',
+							height: '23px',
+							borderRadius: '2px',
+							padding: '3px 7px 3px 7px',
+							lineHeight: 'normal',
+							width: '99.948%',
+							border: '1px solid #a6a6a6',
+							boxShadow: '0 1px 0 rgb(255 255 255 / 50%), 0 1px 0 rgb(0 0 0 / 7%) inset',
+							outline: '0',
+							//borderTopColor: '#949494;',
+							borderColor: '#e77600'
+						}}
+						type="submit"
+						name="submit"
+					/>
 				</div>
 			</div>
 		</Fragment>
