24f13c4a500377448c17ec3efc86d661bee9e580
Jonatahn Bajada <jbajada@ryerson.ca>
2021-06-22 15:34:19 -0700

    added carasoul component

---
+++ client/src/components/Home/components/Carasoul/Carasoul.tsx
@@ -0,0 +1,14 @@
+import React, { Fragment } from 'react';
+
+interface Props {}
+
+const Carasoul: React.FC<Props> = () => {
+	return (
+		<Fragment>
+			<div>
+				<h1>hello</h1>
+			</div>
+		</Fragment>
+	);
+};
+export default Carasoul;
