655bbc0d896c2c94a7f7b1dc10957fae04b753a8
Jonatahn Bajada <jbajada@ryerson.ca>
2021-06-22 15:59:46 -0700

    updated carasoul buttons and css

--- client/src/components/Home/HomePage.tsx
+++ client/src/components/Home/HomePage.tsx
@@ -1,10 +1,12 @@
 import React, { Fragment } from 'react';
 import Nav from '../Nav/Nav';
+import Carasoul from './components/Carasoul/Carasoul';
 
 const HomePage = () => {
 	return (
 		<Fragment>
 			<Nav />
+			<Carasoul />
 		</Fragment>
 	);
 };
--- client/src/components/Home/components/Carasoul/Carasoul.tsx
+++ client/src/components/Home/components/Carasoul/Carasoul.tsx
@@ -1,12 +1,27 @@
 import React, { Fragment } from 'react';
+import { BsChevronLeft } from 'react-icons/bs';
+import { BsChevronRight } from 'react-icons/bs';
+import styles from './Carasoul.module.css';
 
 interface Props {}
 
 const Carasoul: React.FC<Props> = () => {
 	return (
 		<Fragment>
-			<div>
-				<h1>hello</h1>
+			<div className={styles.carasoulContainer}>
+				<div style={{ display: 'flex', justifyContent: 'center' }}>
+					<div className={styles.leftArrow}>
+						<div style={{ display: 'flex', alignSelf: 'center' }}>
+							<BsChevronLeft size={40} style={{ marginRight: '9px' }} />
+						</div>
+					</div>
+
+					<div className={styles.rightArrow}>
+						<div style={{ display: 'flex', alignSelf: 'center' }}>
+							<BsChevronRight size={40} style={{ marginLeft: '9px' }} />
+						</div>
+					</div>
+				</div>
 			</div>
 		</Fragment>
 	);
