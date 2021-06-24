23b717ab4691c2f5e0a41b5ddd7531fdca9045d6
Jonatahn Bajada <jbajada@ryerson.ca>
2021-06-23 21:30:00 -0700

    updated card images

--- client/src/components/Home/components/ProductCardLayout/ProductCardLayout.tsx
+++ client/src/components/Home/components/ProductCardLayout/ProductCardLayout.tsx
@@ -1,6 +1,9 @@
 import React, { Fragment } from 'react';
 import ProductCard from './components/ProductCard/ProductCard';
 import basic from './components/ProductCard/img/basics.jpg';
+import fitAtHome from './components/ProductCard/img/fit.jpeg';
+import oculus from './components/ProductCard/img/oculus.jpg';
+import beauty from './components/ProductCard/img/beauty.jpg';
 
 let link =
 	'https://www.amazon.com/s?k=amazonbasics&pd_rd_r=819237ee-398d-42b4-96aa-30db3d603514&pd_rd_w=kiIoy&pd_rd_wg=Vjot0&pf_rd_p=9349ffb9-3aaa-476f-8532-6a4a5c3da3e7&pf_rd_r=9SGPBYPHG2S9GHYN9QB7&ref=pd_gw_unk';
@@ -13,31 +16,31 @@ const Data = [
 	},
 	{
 		id: '0',
-		name: 'AmazonBasics',
+		name: 'Get fit at home',
+		image: fitAtHome,
+		link: link
+	},
+	{
+		id: '0',
+		name: 'Shop by Category',
 		image: basic,
 		link: link
 	},
 	{
 		id: '0',
-		name: 'AmazonBasics',
-		image: basic,
+		name: 'Oculus',
+		image: oculus,
 		link: link
 	},
 	{
 		id: '0',
-		name: 'AmazonBasics',
-		image: basic,
+		name: 'Beauty picks',
+		image: beauty,
 		link: link
 	},
 	{
 		id: '0',
-		name: 'AmazonBasics',
-		image: basic,
-		link: link
-	},
-	{
-		id: '0',
-		name: 'AmazonBasics',
+		name: 'Gaming accessories',
 		image: basic,
 		link: link
 	}
