3bf0dd0892cb0a6ac16723a0259c009ddd8a10bf
Jonatahn Bajada <jbajada@ryerson.ca>
2021-06-26 00:41:34 -0700

    updated card layout

--- client/src/components/Home/components/Body/HomeBody.tsx
+++ client/src/components/Home/components/Body/HomeBody.tsx
@@ -2,12 +2,14 @@ import React, { Fragment } from 'react';
 import Carasoul from '../Carasoul/Carasoul';
 import ProductCardLayout from '../ProductCardLayout/ProductCardLayout';
 import styles from './HomeBody.module.css';
+import { Grid } from '@material-ui/core';
 
 const Body: React.FC = () => {
 	return (
 		<Fragment>
 			<div className={styles.bodyContainer}>
 				<Carasoul />
+				<ProductCardLayout />
 			</div>
 		</Fragment>
 	);
--- client/src/components/Home/components/ProductCardLayout/ProductCardLayout.tsx
+++ client/src/components/Home/components/ProductCardLayout/ProductCardLayout.tsx
@@ -5,6 +5,7 @@ import fitAtHome from './components/ProductCard/img/fit.jpeg';
 import oculus from './components/ProductCard/img/oculus.jpg';
 import beauty from './components/ProductCard/img/beauty.jpg';
 import styles from './ProductCardLayout.module.css';
+import { Grid } from '@material-ui/core';
 
 let link =
 	'https://www.amazon.com/s?k=amazonbasics&pd_rd_r=819237ee-398d-42b4-96aa-30db3d603514&pd_rd_w=kiIoy&pd_rd_wg=Vjot0&pf_rd_p=9349ffb9-3aaa-476f-8532-6a4a5c3da3e7&pf_rd_r=9SGPBYPHG2S9GHYN9QB7&ref=pd_gw_unk';
@@ -59,30 +60,17 @@ const Data = [
 	}
 ];
 
-interface Props {
-	styleCard: string;
-}
+interface Props {}
 
 const ProductCardLayout: React.FC<Props> = (props) => {
 	return (
 		<Fragment>
-			<div
-				className={props.styleCard}
-				style={{
-					position: 'relative',
-					width: '1500px',
-					minWidth: '1500px',
-					backgroundColor: 'transparent',
-					paddingLeft: '20px',
-					paddingRight: '20px',
-					display: 'flex',
-
-					flexWrap: 'wrap'
-				}}
-			>
-				{Data.map((data, index) => {
-					return <ProductCard name={data.name} link={data.link} image={data.image} />;
-				})}
+			<div className={styles.root}>
+				<Grid container>
+					{Data.map((data, index) => {
+						return <ProductCard name={data.name} link={data.link} image={data.image} />;
+					})}
+				</Grid>
 			</div>
 		</Fragment>
 	);
